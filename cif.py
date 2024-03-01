580041005c16ce4116b8811e0786121964b6cf1f
Malina0180 <105682612+Malina0180@users.noreply.github.com>
2024-03-01 13:20:29 +0100

    Add files via upload

---
+++ cif.py
@@ -0,0 +1,4 @@
+a = int(input("Podaj tempterature w stopniach celsjusza -"))
+b = a * 2 + 30 
+
+print("°F",b , ",°C", a)
---
+++ promienkola.py
@@ -0,0 +1,5 @@
+pi = 3.14
+r = int(input("Podaj wartość promienia okregu -"))
+print(pi*r*2)
+print(2*pi*r)
+
